0bdd45a6084331aa9ced203aadd625a07e21294d
Trincazul <endriw_villa@icloud.com>
2020-07-09 03:06:59 -0300

    Modificado o site

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Sobre A Grine Tey",
+        "excerpt":"Grine Tey é uma organização criada com o intuito de contribuir com projetos Open Source, e também a criação de projetos, unindo pessoas que adoram o mundo do Open Source, se vc tiver interesse em participar dos projetos, entrar em contato com o email endriw_villa@icloud.com Grine Tey is an organization...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/Primeiro-Post/"
+      }]
